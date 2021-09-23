6bfc20b234b6eaaa02b2e70a0dd4551551fd3633
Sergii <sergiy.stotskiy@gmail.com>
2021-09-23 15:10:56 +0300

    fix: bla

---
+++ products/findAll/index.js
@@ -0,0 +1,18 @@
+const { api, corsHeaders } = require('@architect/shared');
+
+exports.handler = async function findProducts() {
+  const response = await api.get('/products.json');
+  const extraHeaders = response.status === 200
+    ? { 'cache-control': 'public, max-age=600, s-maxage=600' }
+    : null;
+
+  return {
+    statusCode: response.status,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      ...extraHeaders,
+      ...corsHeaders(),
+    },
+    body: JSON.stringify(response.data),
+  }
+}
